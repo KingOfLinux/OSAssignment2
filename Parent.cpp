f418cf80d563b4ec2a30dd3edc3b268139d5c4d1
KingOfLinux <shawnco789@gmail.com>
2017-02-11 18:30:13 -0500

    C++ part finished

--- Parent.cpp
+++ Parent.cpp
@@ -8,12 +8,17 @@
 #include <fcntl.h>
 #include <iostream>
 #include <string>
-
 #include <vector>
 #include <fstream>
+#include <cstring>
+#include <memory>
+#include <sstream>
+
+
+
+
 
 using namespace std;
-
 
 int main(int argc, char **argv){
 
@@ -21,6 +26,18 @@ int main(int argc, char **argv){
   pid_t parentPID = getpid();
   cout << "Parent PID: " << parentPID << endl;
 
+  //Making a pipe
+  int fd[2];
+  int READ = 0;
+  int WRITE = 1;
+  pipe(fd);
+  char command[] = "LISTALL";
+  char readbuf[sizeof(command)];
+
+  write(fd[WRITE], command, sizeof(command));
+
+
+  //forking child process
   pid_t childPID = fork();
   if (childPID < 0)
   {
@@ -28,11 +45,43 @@ int main(int argc, char **argv){
   }
   else if(childPID == 0)
   {
-    if (execl("Child", "Child", "LISTALL", NULL) == -1){
-      perror("child exec() error");
+    cout << "Child Executing" << endl;
+    read(fd[READ], readbuf, sizeof(readbuf));
+    cout << "Child recieved: " << readbuf << " from pipe"<< endl;
+
+    pid_t parentPID = getppid();
+    cout << "Parent PID from child: " << parentPID << endl;
+    pid_t childPID = getpid();
+    cout << "Child PID from child: " << childPID << endl;
+
+    string PID_String = to_string(childPID) + " " + to_string(parentPID);
+    cout << PID_String << endl;
+
+    char PID_char[20];
+    strcpy(PID_char, PID_String.c_str());
+
+    write(fd[WRITE],PID_char , sizeof(PID_char));
+
+
   }
-    sleep(2);
-  }
+    sleep(1);
+
+    read(fd[READ], readbuf, sizeof(readbuf)+20);
+    cout << "Parent recieved: " << readbuf << " from pipe"<< endl;
+
+    cout << "Parent Killing ChildPID" << endl;
+    string PID_String(readbuf);
+    stringstream  stream;
+    stream << PID_String;
+    string firstWord;
+    stream >> firstWord;
+    stringstream convert(firstWord);
+    int firstPID;
+    convert >> firstPID;
+
+    if (kill(firstPID, SIGKILL) == -1){
+      perror("Kill failed");
+    }
 
 
   return 0;
